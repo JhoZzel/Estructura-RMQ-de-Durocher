a972a4abb99e99b1ac4463689cfc1a295990c7f8
JhoZzel <joel.lol177@gmail.com>
2025-06-02 00:33:29 -0500

    Eliminacion de random.h

--- random.h
+++
@@ -1,8 +0,0 @@
-#include <random>     // mt19937_64, uniform_int_distribution
-#include <chrono>     // chrono::steady_clock
-
-mt19937_64 rnd(chrono::steady_clock::now().time_since_epoch().count());
-
-int random(int L, int R) {
-    return uniform_int_distribution<int>(L, R)(rnd);
-}
